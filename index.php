cf569e0f36b8d9cdb6a05e70a6714519bd90af55
DominicMorquecho <dominic.morquecho@gmail.com>
2015-05-22 10:57:49 -0700

    first push

---
+++ index.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<link rel="stylesheet" type="text/css" href="css/bootstrap.min.css">
+	<link rel="stylesheet" type="text/css" href="css/main.css">
+	<link href='http://fonts.googleapis.com/css?family=Crimson+Text' rel='stylesheet' type='text/css'>
+	<title>test</title>
+</head>
+<body>
+	<div id="name">
+		Dominic Morquecho
+	</div>
+	<div id="white">
+		
+	</div>
+</body>
+</html>
