62e8fcc7dc2ccc27fdff78ebcfef8dd87d1e7492
DominicMorquecho <dominic.morquecho@gmail.com>
2015-05-26 11:29:57 -0700

    Work

--- index.php
+++ index.php
@@ -4,6 +4,7 @@
 	<link rel="stylesheet" type="text/css" href="css/bootstrap.min.css">
 	<link rel="stylesheet" type="text/css" href="css/main.css">
 	<link href='http://fonts.googleapis.com/css?family=Crimson+Text' rel='stylesheet' type='text/css'>
+	<link href='http://fonts.googleapis.com/css?family=Raleway:600' rel='stylesheet' type='text/css'>
 	<title>test</title>
 </head>
 <body>
@@ -11,7 +12,15 @@
 		Dominic Morquecho
 	</div>
 	<div id="white">
-		
+		<div class="container">
+			<nav class= "col-xs-12" id="nav-senpai">
+				<div class="col-xs-4"></div>
+				<div class="col-xs-4">
+				<a href="Projects.php" id="projects-link">Stuff</a>
+				<a href="Projects.php" id="move-bitch">Contact</a>
+				</div>
+			</nav>
+		</div>
 	</div>
 </body>
 </html>
