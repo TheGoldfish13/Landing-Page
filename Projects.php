bc24770eada2a4763cb94246ef296841909e1c4c
DominicMorquecho <dominic.morquecho@gmail.com>
2015-05-29 11:27:18 -0700

    Made an info page

--- Projects.php
+++ Projects.php
@@ -3,11 +3,26 @@
 <head>
 	<link rel="stylesheet" type="text/css" href="css/bootstrap.min.css">
 	<link rel="stylesheet" type="text/css" href="css/dirty.css">
+	<link href='http://fonts.googleapis.com/css?family=Roboto+Slab' rel='stylesheet' type='text/css'>
+	<link href='http://fonts.googleapis.com/css?family=Arvo' rel='stylesheet' type='text/css'>
 	<title></title>
 </head>
 <body>
+	<nav>
+		<a href="index.php" id="projects-link" class="goaway">Home</a>
+		<a href="Projects.php" id="move-bitch" class="goaway">Contact</a>
+	</nav>
 	<div id="terrible-lies">
-		I am a web developer that has experience coding with html and css, javascript, Jquery, and PHP. <br> I aquired these skills from PHS's APP Academy
+		I am a web developer that has experience coding with <a href="http://www.w3schools.com/html/html_intro.asp">HTML</a>, <a href="http://www.w3schools.com/css/css_intro.asp">CSS</a>, and <a href="http://www.w3schools.com/php/php_intro.asp"> PHP</a>. <br> I aquired these skills from PHS's APP Academy
+	</div>
+	<div class="container">
+		<div class="col-xs-2"></div>
+		<div id="projects" class="col-xs-8">
+			Past Projects <br>
+			<a href="http://localhost/DominicMorquechoAPI/index.php"><img src="img/API.PNG" id="API"></a>
+			<a href="http://localhost/DominicMorquechoAPI/index.php"><img src="img/todo.PNG" id="TODO"></a>
+			<a href="http://localhost/DominicMAwesomenauts/index.php"><img src="img/melon.PNG" id="MELON"></a>
+		</div>
 	</div>
 </body>
 </html>
